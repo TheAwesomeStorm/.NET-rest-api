0ae5d7b38620fef5120cf3eba86e7e4bbe8b1bf8
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-12-29 14:02:46 -0300

    Added FilmeController and Filme model

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> _filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody] Filme filme)
+        {
+            _filmes.Add(filme);
+            Console.WriteLine(filme);
+        }
+
+        [HttpGet]
+        public void Teste()
+        {
+            Console.WriteLine("'oi'");
+        }
+    }
+}
