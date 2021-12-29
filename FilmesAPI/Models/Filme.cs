d2d4cb687a6fd21756e22dc46b5952957962eccb
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-12-29 16:40:51 -0300

    Added attributes annotations to control Filme properties

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,10 +1,16 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Required]
         public string Titulo { get; set; }
+        [Required]
         public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "Limite máximo de caracteres excedido.")]
         public string Genero { get; set; }
+        [Range(1, 600)]
         public int Duracao { get; set; }
     }
 }
